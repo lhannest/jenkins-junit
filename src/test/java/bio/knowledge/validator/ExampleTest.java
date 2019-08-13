89a15e4488c25d6672ed1ea220f7bef0b11cf318
Lance Hannestad <lhannest@sfu.ca>
2019-08-13 09:00:44 -0700

    first commit

---
+++ src/test/java/bio/knowledge/validator/ExampleTest.java
@@ -0,0 +1,24 @@
+package bio.knowledge.validator;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleTest {
+	
+	@Test
+	public void test() {
+		
+	}
+	
+	@Test
+	public void test2() {
+		fail();
+	}
+
+}
